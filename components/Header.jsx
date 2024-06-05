db29f70c7ee80aa374797359c2170a2e854edc1b
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-06-05 16:40:27 +0800

    add nextAuth functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className='shadow-md border-b sticky top-0 bg-white z-30 p-3'>
       <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -31,7 +39,22 @@ const Header = () => {
         />
 
         {/* sign in/nav */}
-        <button className='text-sm font-semibold text-blue-500'>Sign In</button>
+
+        {session ? (
+          <img
+            onClick={() => signOut()}
+            src={session.user.image}
+            alt={session.user.username}
+            className='h-10 w-10 rounded-full cursor-pointer'
+          />
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className='text-sm font-semibold text-blue-500'
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </div>
   );
